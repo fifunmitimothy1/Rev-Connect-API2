6894f48229b6d6fd9ce6b4f03e6fb1f4a3063f40
Benjamin Maymir <benjamin346@revature.net>
2024-08-27 10:11:13 -0500

    Passing happy path tests

--- src/main/java/com/rev_connect_api/services/FollowService.java
+++ src/main/java/com/rev_connect_api/services/FollowService.java
@@ -1,56 +1,56 @@
 package com.rev_connect_api.services;
 
 import java.util.Optional;
-import java.util.Set;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.rev_connect_api.exceptions.InvalidUserException;
+import com.rev_connect_api.models.Follow;
 import com.rev_connect_api.models.User;
+import com.rev_connect_api.repositories.FollowRepository;
 import com.rev_connect_api.repositories.UserRepository;
 
 @Service
 public class FollowService {
   private UserRepository userRepository;
-  public FollowService(UserRepository userRepository) {
+  private FollowRepository followRepository;
+  public FollowService(UserRepository userRepository, FollowRepository followRepository) {
     this.userRepository = userRepository;
+    this.followRepository = followRepository;
   }
 
-  public void followUser(long userId) throws InvalidUserException {
+  public void followUser(long followingId) throws InvalidUserException {
     Optional<User> optionalFollower = userRepository.findByUsername((String)SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-    Optional<User> optionalToFollow = userRepository.findById(userId);
     if(!optionalFollower.isPresent()) {
       throw new InvalidUserException("Authenticated user does not exist");
     }
     User follower = optionalFollower.get();
-    if(!optionalToFollow.isPresent()) {
+    Optional<User> optionalFollowing = userRepository.findById(followingId);
+    if(!optionalFollowing.isPresent()) {
       throw new InvalidUserException("User not found");
     }
-    User toFollow = optionalToFollow.get();
-    Set<User> newFollowing = follower.getFollowing();
-    newFollowing.add(toFollow);
-    follower.setFollowing(newFollowing);
-    userRepository.save(follower);
+    User following = optionalFollowing.get();
+    Follow newFollowRelationship = new Follow(follower, following);
+    followRepository.save(newFollowRelationship);
+
   }
 
-  public void unfollowUser(long userId) throws InvalidUserException {
+  public void unfollowUser(long followingId) throws InvalidUserException {
     Optional<User> optionalFollower = userRepository.findByUsername((String)SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-    Optional<User> optionalToUnfollow = userRepository.findById(userId);
     if(!optionalFollower.isPresent()) {
       throw new InvalidUserException("Authenticated user does not exist");
     }
     User follower = optionalFollower.get();
-    if(!optionalToUnfollow.isPresent()) {
+    Optional<User> optionalFollowing = userRepository.findById(followingId);
+    if(!optionalFollowing.isPresent()) {
       throw new InvalidUserException("User not found");
     }
-    User toUnfollow = optionalToUnfollow.get();
-    if(!follower.getFollowing().contains(toUnfollow)) {
-      throw new InvalidUserException("User is not currently followed");
+    User following = optionalFollowing.get();
+    if(!follower.getFollowing().contains(following)) {
+      return;
     }
-    Set<User> newFollowing = follower.getFollowing();
-    newFollowing.remove(toUnfollow);
-    follower.setFollowing(newFollowing);
-    userRepository.save(follower);
+    Follow followRelationshipToRemove = followRepository.findByFollowerAndFollowed(follower,following).get();
+    followRepository.delete(followRelationshipToRemove);
   }
 }
--- src/test/java/com/rev_connect_api/following/FollowTest.java
+++ src/test/java/com/rev_connect_api/following/FollowTest.java
@@ -1,4 +1,5 @@
 package com.rev_connect_api.following;
+import org.hibernate.Hibernate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,19 +22,27 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import java.net.http.HttpResponse;
 import java.time.LocalDateTime;
 import java.util.Set;
+import java.util.Optional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rev_connect_api.dto.FollowRequestDTO;
 import com.rev_connect_api.models.PersonalProfile;
 import com.rev_connect_api.models.Role;
 import com.rev_connect_api.models.User;
+import com.rev_connect_api.repositories.FollowRepository;
 import com.rev_connect_api.repositories.UserRepository;
 import com.rev_connect_api.security.JwtUtil;
+
+import jakarta.transaction.TransactionScoped;
+import jakarta.transaction.Transactional;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql("/test_states/followTestInit.sql")
 public class FollowTest {
   @Autowired
   private UserRepository userRepository;
+  @Autowired
+  private FollowRepository followRepository;
   @Autowired
   private TestRestTemplate testRestTemplate;
   @Autowired
@@ -47,6 +56,7 @@ public class FollowTest {
 
 
   private final User userInitial, user2Initial, buisnessInitial, userFollowsUser2, user2FollowedByUser, userUnfollowsBuisness, buisnessNotFollowedByUser;
+  private final FollowRequestDTO validFollowDTO, validUnfollowDTO;
 
   public FollowTest() {
     // Create initial state of user
@@ -54,7 +64,7 @@ public class FollowTest {
     userInitial.setUserId(1L);
     userInitial.setUsername("testuser1");
     userInitial.setPassword("$2a$10$PUYTs0ypfVJDNHkheYxqz.1vXx2LlH2pUPub9ipwW0t5ygo9gzQXO");
-    userInitial.setEmail("test1@gmail.com");
+    userInitial.setEmail("test1@mail.com");
     userInitial.setFirstName("test");
     userInitial.setLastName("tester");
     userInitial.setIsBusiness(false);
@@ -63,7 +73,7 @@ public class FollowTest {
     user2Initial.setUserId(2L);
     user2Initial.setUsername("testuser2");
     user2Initial.setPassword("$2a$10$PUYTs0ypfVJDNHkheYxqz.1vXx2LlH2pUPub9ipwW0t5ygo9gzQXO");
-    user2Initial.setEmail("test2@gmail.com");
+    user2Initial.setEmail("test2@mail.com");
     user2Initial.setFirstName("test");
     user2Initial.setLastName("tester");
     user2Initial.setIsBusiness(false);
@@ -72,23 +82,27 @@ public class FollowTest {
     buisnessInitial.setUserId(3L);
     buisnessInitial.setUsername("testuser3");
     buisnessInitial.setPassword("$2a$10$PUYTs0ypfVJDNHkheYxqz.1vXx2LlH2pUPub9ipwW0t5ygo9gzQXO");
-    buisnessInitial.setEmail("test3@gmail.com");
+    buisnessInitial.setEmail("test3@mail.com");
     buisnessInitial.setFirstName("test");
     buisnessInitial.setLastName("tester");
     buisnessInitial.setIsBusiness(true);
     // Establish initil followings
-    userInitial.setFollowing(Set.of(buisnessInitial));
-    buisnessInitial.setFollowers(Set.of(userInitial));
+    // userInitial.setFollowing(Set.of(buisnessInitial));
+    // buisnessInitial.setFollowers(Set.of(userInitial));
     // Create goal states
     userFollowsUser2 = new User(userInitial);
     user2FollowedByUser = new User(user2Initial);
     userUnfollowsBuisness = new User(userInitial);
     buisnessNotFollowedByUser = new User(buisnessInitial);
     // Establish goal followings
-    userFollowsUser2.setFollowing(Set.of(buisnessInitial,user2Initial));
-    user2FollowedByUser.setFollowers(Set.of(userInitial));
-    userUnfollowsBuisness.setFollowing(Set.of());
-    buisnessNotFollowedByUser.setFollowers(Set.of());
+    // userFollowsUser2.setFollowing(Set.of(buisnessInitial,user2Initial));
+    // user2FollowedByUser.setFollowers(Set.of(userInitial));
+    // userUnfollowsBuisness.setFollowing(Set.of());
+    // buisnessNotFollowedByUser.setFollowers(Set.of());
+
+    validFollowDTO = new FollowRequestDTO(2L,"FOLLOW");
+    validUnfollowDTO = new FollowRequestDTO(3L,"UNFOLLOW");
+
 
   }
 
@@ -116,23 +130,53 @@ public class FollowTest {
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(MediaType.APPLICATION_JSON);
     headers.set("Authorization", "Bearer " + token);
-    HttpEntity<PersonalProfile> requestEntity = new HttpEntity<>(null, headers);
+    HttpEntity<FollowRequestDTO> requestEntity = new HttpEntity<>(validFollowDTO, headers);
 
-    ResponseEntity<String> response = testRestTemplate.exchange(serviceLocation + "/" + user2Initial.getUserId(), HttpMethod.POST, requestEntity,String.class);
+    ResponseEntity<String> response = testRestTemplate.exchange(serviceLocation, HttpMethod.POST, requestEntity,String.class);
+    
+    //check server response
 
+    //check status code
     HttpStatusCode statusCode = response.getStatusCode();
     assertEquals(HttpStatus.OK, statusCode, "Expected status code 200. Actual result was " + statusCode.value());
 
-    //check server response
+    // Verify database content
 
-    //Verify database content
-    assertEquals(userFollowsUser2, userRepository.getById(userInitial.getUserId()));
-    assertEquals(user2FollowedByUser, userRepository.getById(user2Initial.getUserId()));
+    // check follow repository for updated records
+    assert(followRepository.findUsersWhoUserFollows(userInitial).contains(user2Initial));
+    assert(followRepository.findUsersWhoFollowUser(user2Initial).contains(userInitial));
+
+    // check if user follows user2 and user2 is followed by user 1
+    assert(userRepository.findById(userInitial.getUserId()).get().getFollowing().contains(user2Initial));
+    assert(userRepository.findById(user2Initial.getUserId()).get().getFollowers().contains(userInitial));
 
   }
 
+  @Test
   public void unfollowUser() {
+    //Generate HTTP request
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    headers.set("Authorization", "Bearer " + token);
+    HttpEntity<FollowRequestDTO> requestEntity = new HttpEntity<>(validUnfollowDTO, headers);
 
+    ResponseEntity<String> response = testRestTemplate.exchange(serviceLocation, HttpMethod.POST, requestEntity,String.class);
+    
+    //check server response
+
+    //check status code
+    HttpStatusCode statusCode = response.getStatusCode();
+    assertEquals(HttpStatus.OK, statusCode, "Expected status code 200. Actual result was " + statusCode.value());
+
+    // Verify database content
+
+    // check follow repository for updated records
+    assert(!followRepository.findUsersWhoUserFollows(userInitial).contains(buisnessInitial));
+    assert(!followRepository.findUsersWhoFollowUser(buisnessInitial).contains(userInitial));
+
+    // check if user follows user2 and user2 is followed by user 1
+    assert(!userRepository.findById(userInitial.getUserId()).get().getFollowing().contains(buisnessInitial));
+    assert(!userRepository.findById(buisnessInitial.getUserId()).get().getFollowers().contains(userInitial));
   }
 
   @Test
